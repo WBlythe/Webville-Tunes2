b0c09683c5715b374dd63bb26aabfbdcb05fa29f
WBlythe <whitneyblythe92@gmail.com>
2016-10-14 00:31:43 -0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,8 +1,8 @@
 window.onload init;
-function init {
+function init() {
 	var button = document.getElementById("addButton");
 	button.onclick = handleButtonClick;
 }
-function handleButtonClick {
+function handleButtonClick() {
 	alert("Button was clicked!");
 }
