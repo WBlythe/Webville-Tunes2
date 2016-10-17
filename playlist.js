c36e823e74ff4fccd4e871605613f1f83c73d1ee
WBlythe <whitneyblythe92@gmail.com>
2016-10-17 15:39:22 -0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -5,4 +5,10 @@ function init() {
 }
 function handleButtonClick() {
 	alert("Button was clicked!");
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	var li = document.createElement("li");
+	li.innerHTML = songName;
+	var ul = document.getElementById("playlist");
+	ul.appendChild(li);
 }
