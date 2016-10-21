b4ede41f9260901d7385da8bb4ea9521b262ab6d
WBlythe <whitneyblythe92@gmail.com>
2016-10-20 18:05:45 -0700

    Update playlist_store.js

--- playlist_store.js
+++ playlist_store.js
@@ -3,11 +3,12 @@ function save(item) {
 	playlistArray.push(item);
 	localStorage.setItem("playlist", JSON.stringify(playlistArray));
 }
+
 function loadPlaylist() {
 	var playlistArray = getSavedSongs();
 	var ul = document.getElementById("playlist");
 	if (playlistArray !=null) {
-		for (var i=0; i<playlistArray.length; i++) {
+		for (var i = 0; i < playlistArray.length; i++) {
 			var li = document.createElement("li");
 			li.innerHTML = playistArray[i];
 			ul.appendChild(li);
@@ -21,7 +22,7 @@ function getSavedSongs() {
 
 function getStoreArray(key) {
 	var playlistArray = localStorage.getItem(key);
-	if (playlistArray == null || playlistArray == "") {
+	if (playlistArray == null || playlistArray == " ") {
 		playlistArray = new Array();
 	}
 	else {
