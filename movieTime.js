c22cbc81ece1a90864efb9ef4d37758f39b25ed7
WBlythe <whitneyblythe92@gmail.com>
2016-10-25 00:47:26 -0700

    Update movieTime.js

--- movieTime.js
+++ movieTime.js
@@ -1,2 +1,34 @@
 function getNextShowing(movie) {
-	var now = 
+	var now = new Date() .getTime();
+	
+	for (var i = 0; i < movie.showtimes.length; i++) {
+		var showtime = getTimeFromString(movie.showtimes[i]);
+		if ((showtime - now) > 0) {
+			return "Next showing of " + movie.title + " is " + movie.showtimes[i];
+		}
+	}
+	return null;
+}
+
+function getTimeFromString(timeString) {
+	var theTime = new Date();
+	var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+	theTime.setHours( parseInt(time[1]) + (time[3] ? 12 : 0 );
+	theTime.setMinutes( parseInt(time[2]) || 0);
+	return theTime.getTime();
+}
+
+var nextShowing = getNextShowing(movie1);
+alert(nextShowing);
+nextShowing = getNextShowing(movie2);
+alert(nextShowing);
+
+var banzaiMovie = {
+	title: "Buckaroo Banzai",
+	genre: "Cult Classic",
+	rating: 5,
+	showtimes: ["1:00pm", "5:00pm", "7:00pm"]
+};
+
+var nextShowing = getNextShowing(banzaiMovie);
+alert(nextShowing);
