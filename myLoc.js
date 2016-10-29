77da1b4e202886f91a4475e8f83074c459027194
WBlythe <whitneyblythe92@gmail.com>
2016-10-29 10:20:35 -0700

    Update myLoc.js

--- myLoc.js
+++ myLoc.js
@@ -1,1 +1,17 @@
 window.onload = getMyLocation;
+
+function (getMyLocation) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayLocation);
+  } else {
+    alert("Oops. No Geolocation support");
+  }
+}
+
+function displayLocation(position) {
+  var latitude = position.coordinates.latitude;
+  var longitude = position.coordinates.longitude;
+  
+  var div = document.getElementById("location");
+  div.innerHTML = "Your are at a latitude: " + latitude + ", Longitude: " + longitude;
+}
