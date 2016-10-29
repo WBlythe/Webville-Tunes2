bf8bb80e1f0e971bda1ac1016655668d6e43518f
WBlythe <whitneyblythe92@gmail.com>
2016-10-29 12:56:59 -0700

    Update myLoc.js

--- myLoc.js
+++ myLoc.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation);
+    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
   } else {
     alert("Oops. No Geolocation support");
   }
@@ -14,4 +14,19 @@ function displayLocation(position) {
   
   var div = document.getElementById("location");
   div.innerHTML = "Your are at a latitude: " + latitude + ", Longitude: " + longitude;
+}
+
+function (displayError) {
+     var errorTypes = {
+     0: "Unknown",
+     1: "Permission denies by user",
+     2: "Position is not available",
+     3: "Request timed out",
+   };
+  var errorMessage = errorTypes[error.code];
+  if (error.code == 0 || error.code == 2) {
+    errorMessage = errorMessage + " " + error.message;
+  }
+  var div = document.getElementById("location");
+  div.innerHTML = errorMessage;
 }
