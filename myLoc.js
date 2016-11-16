d7903647448759b2aca4b7c30be131f509c2e229
WBlythe <whitneyblythe92@gmail.com>
2016-11-16 01:56:13 -0800

    Update myLoc.js

--- myLoc.js
+++ myLoc.js
@@ -1,5 +1,13 @@
 window.onload = getMyLocation;
 
+//global Variable(s)
+
+var ourCoords = {
+  latitude = 47.624851,
+  longitude = -122.52099
+};
+
+//getting my location 
 function getMyLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayLocation, displayError);
@@ -8,14 +16,20 @@ function getMyLocation() {
   }
 }
 
+//displaying location with success Handler
 function displayLocation(position) {
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
   
   var div = document.getElementById("location");
   div.innerHTML = "Your are at a latitude: " + latitude + ", Longitude: " + longitude;
+  
+  var km = computeDistances(position.coords, ourCoords);
+  var distance = document.getElementById("distance");
+  distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
 }
 
+//debugging/error Handler if location is not provided 
 function displayError(error) {
      var errorTypes = {
      0: "Unknown",
@@ -31,6 +45,7 @@ function displayError(error) {
   div.innerHTML = errorMessage;
 }
 
+//Obtaining the distance between two points 
 function computeDistances(startCoords, destCoords) {
   var startLatRads = degreesToRadians(startCoords.latitude);
   var startLongRads = degreesToRadians(startCoords.longitude);
@@ -48,3 +63,5 @@ function degreesToRadians(degrees) {
   var radians = (degrees * Math.PI)/180;
   return radians;
 }
+
+
