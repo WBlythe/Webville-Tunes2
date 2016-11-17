1e994aeede9808a2d14389a8ac2c86eb86c2b10d
WBlythe <whitneyblythe92@gmail.com>
2016-11-17 15:46:20 -0800

    Update myLoc.js

--- myLoc.js
+++ myLoc.js
@@ -1,83 +1,92 @@
+var ourCoords =  {
+	latitude: 47.624851,
+	longitude: -122.52099
+};
+
 window.onload = getMyLocation;
 
-//global Variable(s)
-var ourCoords = {
-  latitude: 47.625851,
-  longitude: -122.52099
-};
+function getMyLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+	} else {
+		alert("Oops, no geolocation support");
+	}
+}
+
+
+
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var div = document.getElementById("location");
+	div.innerHTML = "You are at latitude: " + latitude + " , Longitude: " + longitude;
+
+	var km = computeDistance(position.coords, ourCoords);
+	
+	var distance = document.getElementById("distance");
+	
+	distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
+
+
+
+	showMap(position.coords);
+}
+
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	
+	
+var Radius = 6371; // radius of the earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+			Math.cos(startLatRads) * Math.cos(destLatRads) *
+			Math.cos(startLongRads - destLongRads)) * Radius;;
+
+
+	return distance;
+
+}
+
+	function degreesToRadians(degrees) {
+		 radians = (degrees * Math.PI)/180;
+	return radians;
+}
 
 var map;
 
-//getting my location 
-function getMyLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
-  } else {
-    alert("Oops. No Geolocation support");
-  }
-}
-
-//displaying location with success Handler
-function displayLocation(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  
-  var div = document.getElementById("location");
-  div.innerHTML = "Your are at a latitude: " + latitude + ", Longitude: " + longitude;
-  
-  var km = computeDistance(position.coords, ourCoords);
-  var distance = document.getElementById("distance");
-  distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
-  
-  showMap(position.coords);
-}
-
-//debugging/error Handler if location is not provided 
-function displayError(error) {
-     var errorTypes = {
-     0: "Unknown",
-     1: "Permission denies by user",
-     2: "Position is not available",
-     3: "Request timed out",
-   };
-  var errorMessage = errorTypes[error.code];
-  if (error.code == 0 || error.code == 2) {
-    errorMessage = errorMessage + " " + error.message;
-  }
-  var div = document.getElementById("location");
-  div.innerHTML = errorMessage;
-}
-
-//Obtaining the distance between two points 
-function computeDistance(startCoords, destCoords) {
-  var startLatRads = degreesToRadians(startCoords.latitude);
-  var startLongRads = degreesToRadians(startCoords.longitude);
-  var destLatRads = degreesToRadians(startCoords.latitude);
-  var destLongRads = degreesToRadians(startCoords.longitude);
-  
-  var Radius = 6371; // radius of the earth in km
-  var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
-                           Math.cos(startLatRads) * Math.cos(destLatRads) *
-                           Math.cos(startLongRads - destLongRads)) * Radius;
-  return distance;
-}
-
-function degreesToRadians(degrees) {
-  var radians = (degrees * Math.PI)/180;
-  return radians;
-}
-
-//Displaying the map 
 function showMap(coords) {
-  var googleLatAndLong = 
-      new google.maps.LatLng(coords.latitude,
-                             coords.longitude);
-  
-  var mapOptions = {
-    zoom: 10,
-    center: googleLatAndLong,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var mapDiv = document.getElementById("map");
-  map = new google.maps.Map(mapDiv, mapOptions);
+	var googleLatAndLong =
+		new google.maps.LatLng(coords.latitude,coords.longitude);
+
+	var mapOptions = {
+		zoom: 10,
+		center: googleLatAndLong,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var mapDiv = document.getElementById("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
+}
+
+
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied by the user",
+		2: "Position is not available",
+		3: "Request timed out"
+	};
+
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		errorMessage = errorMessage + " " + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
 }
