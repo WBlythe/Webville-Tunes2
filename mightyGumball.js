315e00a094ea55ecef93f471dec3755e4668a015
WBlythe <whitneyblythe92@gmail.com>
2016-12-16 19:34:23 -0800

    Update mightyGumball.js

--- mightyGumball.js
+++ mightyGumball.js
@@ -1,1 +1,16 @@
-Mighty Gumball
+window.onload = function() {
+  var url = "http://localhost/sales.json";
+  var request = new XMLHttpRequest;
+  request.open("GET", url);
+  request.onload = function() {
+    if(request.status == 200) {
+      updateSales(request.responseText);
+    }
+  };
+  request.send(null);
+}
+
+function updateSales(responseText) {
+  var salesDiv = document.getElementById("sales");
+  salesDiv.innerHTML = reponseText;
+}
